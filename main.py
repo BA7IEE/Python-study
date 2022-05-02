75dba3a0063c597d529c67659bd40bfa87c1674c
tungwerl <tungwerl@gmail.com>
2022-05-02 16:55:42 +0800

    update

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# 这是一个示例 Python 脚本。
+# -*- coding:utf-8 -*-
+# @FileName  :条件语句.py
+# @Time      :2022/5/1 下午10:00
+# @Author    :tungwerl
+def eval_help():
+    '''
+    eval() 函数可以去掉字符串的引号
+    :return: 返回去掉引号的数字（int）
+    '''
+    num = eval(input('请输入数字：'))
+    print(num)
+    return num
 
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
 
-
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
-
-
-# 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+    pass
